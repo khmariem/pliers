787e75cf5d4a491abbadcc7ac5565167e0eb829e
Tal Yarkoni <tyarkoni@gmail.com>
2018-01-04 15:50:35 -0600

    make sure install script cleans branch

--- install_docs.sh
+++ install_docs.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# Make sure we always invoke this script from the gh-pages branch
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" != "gh-pages" ]]; then
+  echo 'The doc installation script must be run from the gh-pages branch!';
+  exit 1;
+fi
+
+git clean -fdx
 # git clone --no-checkout --depth 1 https://github.com/tyarkoni/pliers.git docs
 git checkout sphinx-docs docs
-ls
 cd docs
 make clean
-TIMESTAMP=$(date)
 make html
-ls _build/html/*
+TIMESTAMP=$(date)
 cp -R _build/html/* ..
 cd ..
-ls
 rm -rf docs
 git add .
 git commit -m "pliers docs built with Sphinx on $TIMESTAMP"
